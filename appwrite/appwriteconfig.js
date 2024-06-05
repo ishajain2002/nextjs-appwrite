b93f64ccff1ed57305a50d6909ccc0f2b9a2bb47
Isha jain <92178327+ishajain2002@users.noreply.github.com>
2024-06-05 15:38:59 +0530

    Update appwriteconfig.js

--- appwrite/appwriteconfig.js
+++ appwrite/appwriteconfig.js
@@ -2,6 +2,6 @@ import { Client, Account, Databases } from 'appwrite';
 const client = new Client();
 client
     .setEndpoint('https://cloud.appwrite.io/v1')
-    .setProject('66597cea00333e92c0a9');
+    .setProject('666038ab00035cd3c9be');
 
 export const account= new Account(client);
